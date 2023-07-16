3de0a7e6de1c392ea85845e33c0c17125b60f4c2
Denperidge <denperidge@gmail.com>
2023-07-16 02:01:08 +0200

    musixmatch.js all sections parsing, added Deli Girls - mf

--- utils/musixmatch.js
+++ utils/musixmatch.js
@@ -2,10 +2,11 @@
 Extracts & parses the sync into text
 (WIP)
 
-Step 1: Inspect Element, click on any of the lyrics
-Step 2: Go up a parent element until you find the first element that contains all lyrics
-Step 3: Right click that element, select "Use in console"
-Step 4: Run the following code
+Step 1: Go to the Sync tab. Try not to open the Tag tab, it seems to goof it up
+Step 2: Inspect Element, click on any of the lyrics
+Step 3: Go up a parent element until you find the first element that contains all lyrics
+Step 4: Right click that element, select "Use in console"
+Step 5: Run the following code
 */
 
-temp0.innerText.replace("...", "").replace("End of lyrics", "").replace(/^Verse/gm, "\n#Verse").replace(/^Chorus/gm, "\n#Chorus").replace(/\n(?!\d{1,2}:\d{1,2}.\d{1,2}|#)/gm, "\t")
+temp0.innerText.replace("...", "").replace("End of lyrics", "").replace(/\n^(?=Intro|Verse|Pre-Chorus|Chorus|Hook|Bridge|Outro)/gmi, "\n#").replace(/\n(?!\d{1,2}:\d{1,2}.\d{1,2}|#)/gm, "\t")
